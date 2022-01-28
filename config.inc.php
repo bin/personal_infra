d3bef1f0192d8d5e1f7d1851cea21f0f92b9a674
Jackalope <jack@seatgull.com>
2022-01-27 23:04:37 -0600

    init

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php
+$config['db_dsnw'] = 'mysql://roundcube_user:no_creds_4_u@localhost/roundcube_db';
+$config['default_host'] = 'ssl://mail.datacowboy.cf';
+$config['default_port'] = 993;
+$config['smtp_server'] = 'localhost';
+$config['smtp_port'] = 25;
+$config['support_url'] = 'mailto:bin@datacowboy.cf';
+$config['ip_check'] = true;
+$config['des_key'] = '0123makesuretoreplaceme';
+$config['product_name'] = 'datacowboy.cf Webmail';
+$config['plugins'] = array();
+$config['language'] = 'en_US';
+$config['mail_pagesize'] = 100;
+$config['addressbook_pagesize'] = 100;
+$config['htmleditor'] = 2;
+$config['draft_autosave'] = 60;
